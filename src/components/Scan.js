3a44b5ad5bfcfea957f109492d11cd771ba7a6d3
Kaushal Jha <you@example.com>
2024-02-23 15:15:03 +0530

    Sanned url working Fine

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -1,36 +1,70 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
-import {useCameraDevice, useCameraPermission , useCodeScanner ,Camera}  from 'react-native-vision-camera';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useCameraDevice, useCameraPermission, useCodeScanner, Camera } from 'react-native-vision-camera';
+import WebView from 'react-native-webview';
 
 const Scan = () => {
-  const {hasPermission, requestPermission} = useCameraPermission();
-  const device = useCameraDevice('back');
+    const { hasPermission, requestPermission } = useCameraPermission();
+    const device = useCameraDevice('back');
+    const [linkDetected, setLinkDetected] = useState(false);
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
+    useEffect(() => {
+        requestPermission();
+    }, []);
 
-  const codeScanner = useCodeScanner({
-    codeTypes: ['qr', 'ean-13'],
-    onCodeScanned: codes => {
-      console.log(`Scanned ${codes[0].value} codes!`);
-    },
-  });
-  if (device == null) {
+    const navigateToURLInWebView = (url) => {
+       
+        return (
+            <WebView
+                source={{ uri: url }}
+                style={{ flex: 1 }}
+            />
+        );
+    };
+
+    const codeScanner = useCodeScanner({
+        codeTypes: ['qr', 'ean-13'],
+        onCodeScanned: codes => {
+            if (!linkDetected) {
+                const scannedLink = codes[0].value;
+                Alert.alert(
+                    'Scanned Link',
+                    `Do you want to navigate to this link?\n${scannedLink}`,
+                    [
+                        {
+                            text: 'Cancel',
+                            onPress: () => console.log('Cancel Pressed'),
+                            style: 'cancel',
+                        },
+                        {
+                            text: 'OK', onPress: () => {
+                                setLinkDetected(true);
+                                navigateToURLInWebView(scannedLink);
+                            }
+                        },
+                    ],
+                    { cancelable: false },
+                );
+            }
+        },
+    });
+
+    if (device == null) {
+        return (
+            <View>
+                <Text>Device Not Found</Text>
+            </View>
+        );
+    }
+
     return (
-      <View>
-        <Text>Device NOt Found</Text>
-      </View>
+        <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={true}
+            codeScanner={codeScanner}
+        />
     );
-  }
-  return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={true}
-      codeScanner={codeScanner}
-    />
-  );
 };
 
 export default Scan;
